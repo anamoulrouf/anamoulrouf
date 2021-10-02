269e201e4d944b844162afac0fe95fde2316efde
Anamoul Rouf <rajeebrouf.bd@gmail.com>
2021-10-02 19:35:55 +0600

    Responsive Update v1.4
    Responsive code updated!

--- js/main-1.js
+++ js/main-1.js
@@ -1,12 +1,13 @@
 
 		// jQuery to collapse the navbar on scroll
 		$(window).scroll(function() {
-		    if ($("nav").offset().top > 30) {
+		    if ($("nav").offset().top > 24) {
 		        $("nav").addClass("top-nav-collapse");
 		    } else {
 		        $("nav").removeClass("top-nav-collapse");
 		    }
 		});
 		
+
 
 
