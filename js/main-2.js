0b5d77ae8c2696b25283d89b90e85586037232ef
Anamoul Rouf <rajeebrouf.bd@gmail.com>
2025-05-25 00:49:19 +0600

    new update

--- js/main-2.js
+++ js/main-2.js
@@ -1,3 +1,5 @@
+// Marquee Scroll
+
 const root = document.documentElement;
 const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
 const marqueeContent = document.querySelector("ul.marquee-content");
@@ -7,3 +9,4 @@ root.style.setProperty("--marquee-elements", marqueeContent.children.length);
 for(let i=0; i<marqueeElementsDisplayed; i++) {
   marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
 }
+
